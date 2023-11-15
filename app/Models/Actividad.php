4785f22ce19fb683e58f42ca599e5933167ba26c
Christian Celso <christiancelsomamanisoliz0@gmail.com>
2023-11-15 14:31:14 -0400

    servicio actividad y ingreso

---
+++ app/Models/Actividad.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Actividad extends Model
+{
+    use HasFactory;
+    protected $table = "actividades";
+    protected $fillable = ['nombre','fecha', 'horafin', 'horainicio', 'montototal'];
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\ActividadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\IngresoController;
 use App\Http\Controllers\Api\PhotoController;
 use App\Http\Controllers\Api\MiembroController;
 
@@ -24,4 +26,22 @@ Route::prefix('miembro')->group(function () {
     Route::get("/obtener/{id}", [MiembroController::class, "obtener"]);
     Route::put("/editar/{id}", [MiembroController::class, "editar"]);
     Route::delete("/eliminar/{id}", [MiembroController::class, "eliminar"]);
+});
+
+//gestionar actividad
+Route::prefix('actividad')->group(function () {
+    Route::get("/listar", [ActividadController::class, "listar"]);
+    Route::post("/registrar", [ActividadController::class, "registrar"]);
+    Route::get("/obtener/{id}", [ActividadController::class, "obtener"]);
+    Route::put("/editar/{id}", [ActividadController::class, "editar"]);
+    Route::delete("/eliminar/{id}", [ActividadController::class, "eliminar"]);
+});
+
+//gestionar ingreso
+Route::prefix('ingreso')->group(function () {
+    Route::get("/listar", [IngresoController::class, "listar"]);
+    Route::post("/registrar", [IngresoController::class, "registrar"]);
+    Route::get("/obtener/{id}", [IngresoController::class, "obtener"]);
+    Route::put("/editar/{id}", [IngresoController::class, "editar"]);
+    Route::delete("/eliminar/{id}", [IngresoController::class, "eliminar"]);
 });
